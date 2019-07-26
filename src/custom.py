f2289594928e09a4997e34d974b08b543e282f04
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-26 18:31:48 -0400

    Added top_100 to clean_drm_ec. Added functions to custom.py to get ALS recommendations, user cluster, and user ratings. Fixed get_recommendations function in custom.py

--- src/custom.py
+++ src/custom.py
@@ -4,7 +4,7 @@
 from scipy.spatial import distance_matrix
 import pandas as pd
 import numpy as np
-
+from joblib import load
 
 def test_custom():
     print("In custom module")
@@ -45,7 +45,9 @@ def top_rated_movies(cluster, centroids, item_factors_unstacked):
     return top_movies.title
 
 
-def get_recommendations(user_cluster, centroids, item_factors_unstacked):
+def get_recommendations(user_cluster):
+    centroids = pd.read_csv('../data/processed/centroids.csv', index_col=['Unnamed: 0'])
+    item_factors_unstacked = pd.read_csv('../data/processed/item_factors_unstacked.csv', index_col=['id'])
     cluster_distance_df = cluster_distances(centroids)
     near_clusters = nearest_clusters(cluster_distance_df, user_cluster)
     recommendation_set = set()
@@ -58,3 +60,56 @@ def get_recommendations(user_cluster, centroids, item_factors_unstacked):
             recs = np.random.choice(list(cluster_unique_top_movies), size=4, replace=False)
             recommendation_set.update(recs)
     print(list(recommendation_set))
+
+
+def ALS_recommendations(user_ratings):
+    rated_movies = [float(x[0]) for x in user_ratings]
+    item_factors_pdf = pd.read_csv('../data/processed/item_factors.csv', index_col='Unnamed: 0')
+    rated_item_factor = item_factors_pdf.loc[item_factors_pdf['id'].isin(rated_movies)].pivot(index='id', columns='value', values='features')
+    M = rated_item_factor.as_matrix()
+    E = np.identity(42)
+    nui = len(rated_movies)
+    regParam = 0.15
+    R = np.array([float(x[1]) for x in user_ratings])
+    A = M.T.dot(M)+regParam*nui*E
+    V = M.T.dot(R.T)
+    user_fac = np.linalg.inv(A).dot(V)
+    item_factors_unstacked = pd.read_csv('../data/processed/item_factors_unstacked.csv', index_col=['id'])
+    user_movie_ratings = user_fac.dot(item_factors_unstacked.T)
+    user_movie_ratings_df = pd.DataFrame(user_movie_ratings)
+    user_movie_ratings_df['movieId'] = item_factors_unstacked.T.columns
+    user_top_10 = user_movie_ratings_df.sort_values(0,ascending=False).head(10)
+    movies_df = pd.read_csv('../data/raw/movies.csv')
+    user_top_10 = user_top_10.merge(movies_df, how='left', on='movieId')
+    user_top_10.drop([0,'movieId', 'genres'], axis=1, inplace=True)
+    print(list(user_top_10.title))
+    return user_fac
+
+
+def get_user_cluster(user_fac):
+    gbc = load('../models/fifp_classification.joblib') 
+    user_cluster = gbc.predict(user_fac.reshape(1, -1))[0]
+    return user_cluster
+
+
+def user_rankings():    
+    top_100 = pd.read_csv('../data/processed/top_100.csv', index_col='Unnamed: 0')
+    ranking_list = top_100.sample(n=50, axis=0)
+    user_ratings = []
+    i = 0
+    j = 0
+    while j<9:
+        title = ranking_list['title']
+        movieId = ranking_list['movieId']
+        #print(title.iloc[i])
+        user_rating = input('Enter a ranking for {} from 1 (lowest) to 5 (highest). If you have not seen the movie, press enter.'.format(title.iloc[i]))
+        i +=1
+        j = len(user_ratings)
+        if user_rating=='':
+            pass
+        elif float(user_rating) in range(0,6):
+            user_ratings.append((movieId.iloc[i], user_rating))
+        else:
+            print('Ratings must be whole numbers between 1 and 5.')
+            pass
+    return user_ratings
