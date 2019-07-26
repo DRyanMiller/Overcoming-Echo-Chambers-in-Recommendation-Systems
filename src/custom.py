040ad90bb687d5805354c6c696f248ac82648ddc
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-26 16:04:32 -0400

    Added code for cleaning and separating the movie.csv data

--- src/custom.py
+++ src/custom.py
@@ -3,6 +3,7 @@
 # custom.py
 from scipy.spatial import distance_matrix
 import pandas as pd
+import numpy as np
 
 
 def test_custom():
@@ -10,16 +11,10 @@ def test_custom():
     return None
 
 
-def cluster_centroids(clustered_data):
-    """Calculates the centroids of each cluster of users"""
-    cluster_centroids = clustered_data.groupby(['cluster']).agg('mean')
-    return centroids
-
-
-def cluster_distances_df(centroids):
-    centroids_array = centroids.to_numpy()
-    cluster_distance_matrix = distance_matrix(centroids_array,
-                                              centroids_array, p=2)
+def cluster_distances(centroids):
+    #centroids_array = centroids.to_numpy()
+    cluster_distance_matrix = distance_matrix(centroids,
+                                              centroids, p=2)
     cluster_distance_df = pd.DataFrame(cluster_distance_matrix)
     return cluster_distance_df
 
@@ -29,36 +24,37 @@ def nearest_clusters(cluster_distance_df, cluster, num_nearest_clusters=2):
     return sorted_distances[1:num_nearest_clusters+1].index.values.astype(int)
 
 
-def centroid_ratings(cluster_centroids, item_factors_unstacked_transposed):
-    centroid_ratings = np.dot(cluster_centroids, item_factors_unstacked_transposed)
+def centroid_ratings(centroids, item_factors_unstacked):
+    item_factors_unstacked_transposed = item_factors_unstacked.T
+    centroid_ratings = np.dot(centroids, item_factors_unstacked_transposed)
     centroid_ratings_df = pd.DataFrame(centroid_ratings)
     centroid_ratings_df.columns = item_factors_unstacked.index
     centroid_ratings_T_df = centroid_ratings_df.transpose()
     return centroid_ratings_T_df
 
 
-def top_rated_movies(cluster, centroid_ratings_T_df):
+def top_rated_movies(cluster, centroids, item_factors_unstacked):
+    centroid_ratings_T_df = centroid_ratings(centroids, item_factors_unstacked)
     sorted_ratings = centroid_ratings_T_df[cluster].sort_values(ascending=False)
     sorted_ratings_df = sorted_ratings.reset_index()
-    most_rated_df = pd.merge(most_rated, movies_df, how='left', on='movieId')
-    most_rated_df.drop(['total_recs', 0], axis=1, inplace=True)
-    top_movies = pd.merge(sorted_ratings_df, most_rated_df, how='inner', left_on='id', right_on='movieId')
-    top_movies.columns = ['id', 'rating', 'movieId', 'title', 'genres']
-    top_10_movies = top_movies.sort_values(by='rating', ascending=False )[:10]
-    return top_10_movies.title
+    sorted_ratings_df.columns = ['id', 'rating']
+    most_rated = pd.read_csv('../data/processed/most_rated.csv', index_col='Unnamed: 0')
+    top_movies = pd.merge(sorted_ratings_df, most_rated, how='inner', left_on='id', right_on='movieId')
+    #top_movies.columns = ['id', 'rating', 'movieId', 'title', 'genres']
+    #top_100_movies = top_movies.sort_values(by='rating', ascending=False )[:100]
+    return top_movies.title
 
 
-def get_recommendations(user_cluster, clustered_data):
-    centroids = cluster_centroids(clustered_data)
-    cluster_distance_df = cluster_distances_df(centroids)
-    near_clusters = nearest_clusters(user_cluster)
+def get_recommendations(user_cluster, centroids, item_factors_unstacked):
+    cluster_distance_df = cluster_distances(centroids)
+    near_clusters = nearest_clusters(cluster_distance_df, user_cluster)
     recommendation_set = set()
     for index, cluster in enumerate(near_clusters):
         if index==0:
-            recs = np.random.choice(top_rated_movies(cluster), size=6, replace=False)
+            recs = np.random.choice(top_rated_movies(cluster, centroids, item_factors_unstacked), size=6, replace=False)
             recommendation_set.update(set(recs))
         if index==1:
-            cluster_unique_top_movies = set(top_rated_movies(cluster)).difference(recommendation_set)
+            cluster_unique_top_movies = set(top_rated_movies(cluster, centroids, item_factors_unstacked)).difference(recommendation_set)
             recs = np.random.choice(list(cluster_unique_top_movies), size=4, replace=False)
             recommendation_set.update(recs)
     print(list(recommendation_set))
