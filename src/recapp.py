a710c431251bb8f992fa8d38b512c2e90798746e
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-29 13:38:30 -0400

    Corrects format of function, file, and variable names across notebooks and py files.

--- src/recapp.py
+++ src/recapp.py
@@ -1,12 +1,12 @@
 from src import custom as cm
 
 
-def recommendations():
-    user_ratings = cm.user_rankings()
-    user_fac = cm.user_factors(user_ratings)
-    ALS_recs = cm.ALS_recommendations(user_fac)
+def get_recommendations():
+    user_ratings = cm.get_user_rankings()
+    user_fac = cm.get_user_factors(user_ratings)
+    ALS_recs = cm.get_als_recommendations(user_fac)
     user_cluster = cm.get_user_cluster(user_fac)
-    new_recs = cm.get_recommendations(user_cluster)
+    new_recs = cm.get_new_recommendations(user_cluster)
     print('\n\n\n')
     print('Try:')
     for movie in ALS_recs: print('    {}'.format(movie), sep = "\n")
