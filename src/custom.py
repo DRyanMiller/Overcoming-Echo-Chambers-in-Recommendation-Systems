dffb334aef6887f7f16732274cd48cbedbcfe970
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-29 12:34:21 -0400

    Erased unneed .py files and linted remaining .py files

--- src/custom.py
+++ src/custom.py
@@ -13,7 +13,6 @@ def test_custom():
 
 
 def cluster_distances(centroids):
-    #centroids_array = centroids.to_numpy()
     cluster_distance_matrix = distance_matrix(centroids,
                                               centroids, p=2)
     cluster_distance_df = pd.DataFrame(cluster_distance_matrix)
@@ -39,34 +38,55 @@ def top_rated_movies(cluster, centroids, item_factors_unstacked):
     sorted_ratings = centroid_ratings_T_df[cluster].sort_values(ascending=False)
     sorted_ratings_df = sorted_ratings.reset_index()
     sorted_ratings_df.columns = ['id', 'rating']
-    most_rated = pd.read_csv('../data/processed/most_rated.csv', index_col='Unnamed: 0')
-    top_movies = pd.merge(sorted_ratings_df, most_rated, how='inner', left_on='id', right_on='movieId')
-    #top_movies.columns = ['id', 'rating', 'movieId', 'title', 'genres']
-    #top_100_movies = top_movies.sort_values(by='rating', ascending=False )[:100]
+    most_rated = pd.read_csv('../data/processed/most_rated.csv',
+                             index_col='Unnamed: 0')
+    top_movies = pd.merge(sorted_ratings_df, most_rated,
+                          how='inner',
+                          left_on='id',
+                          right_on='movieId')
+    # top_100_movies = top_movies.sort_values(by='rating',
+    #                                         ascending=False )[:100]
     return top_movies.title
 
 
 def get_recommendations(user_cluster):
-    centroids = pd.read_csv('../data/processed/centroids.csv', index_col=['Unnamed: 0'])
-    item_factors_unstacked = pd.read_csv('../data/processed/item_factors_unstacked.csv', index_col=['id'])
+    centroids = pd.read_csv('../data/processed/centroids.csv',
+                            index_col=['Unnamed: 0'])
+    item_factors_unstacked = pd.read_csv('../data/processed/\
+                                         item_factors_unstacked.csv',
+                                         index_col=['id'])
     cluster_distance_df = cluster_distances(centroids)
     near_clusters = nearest_clusters(cluster_distance_df, user_cluster)
     recommendation_set = set()
     for index, cluster in enumerate(near_clusters):
-        if index==0:
-            recs = np.random.choice(top_rated_movies(cluster, centroids, item_factors_unstacked), size=6, replace=False)
+        if index == 0:
+            recs = np.random.choice(top_rated_movies(cluster, centroids,
+                                                     item_factors_unstacked),
+                                    size=6,
+                                    replace=False)
             recommendation_set.update(set(recs))
-        if index==1:
-            cluster_unique_top_movies = set(top_rated_movies(cluster, centroids, item_factors_unstacked)).difference(recommendation_set)
-            recs = np.random.choice(list(cluster_unique_top_movies), size=4, replace=False)
+        if index == 1:
+            cluster_unique_top_movies = set(top_rated_movies(cluster,
+                                                             centroids,
+                                                             item_factors_unstacked))\
+                                            .difference(recommendation_set)
+            recs = np.random.choice(list(cluster_unique_top_movies),
+                                    size=4,
+                                    replace=False)
             recommendation_set.update(recs)
     return list(recommendation_set)
+
 
 def user_factors(user_ratings):
     user_ratings.sort()
     rated_movies = [float(x[0]) for x in user_ratings]
-    item_factors_pdf = pd.read_csv('../data/processed/item_factors.csv', index_col='Unnamed: 0')
-    rated_item_factor = item_factors_pdf.loc[item_factors_pdf['id'].isin(rated_movies)].pivot(index='id', columns='value', values='features')
+    item_factors_pdf = pd.read_csv('../data/processed/item_factors.csv',
+                                   index_col='Unnamed: 0')
+    rated_item_factor = item_factors_pdf.loc[item_factors_pdf['id']
+                                             .isin(rated_movies)]\
+                                        .pivot(index='id',
+                                               columns='value',
+                                               values='features')
     M = rated_item_factor.values
     E = np.identity(42)
     nui = len(rated_movies)
@@ -77,41 +97,47 @@ def user_factors(user_ratings):
     user_fac = np.linalg.inv(A).dot(V)
     return user_fac
 
+
 def ALS_recommendations(user_fac):
-    item_factors_unstacked = pd.read_csv('../data/processed/item_factors_unstacked.csv', index_col=['id'])
+    item_factors_unstacked = pd.read_csv('../data/processed/\
+                                         item_factors_unstacked.csv',
+                                         index_col=['id'])
     user_movie_ratings = user_fac.dot(item_factors_unstacked.T)
     user_movie_ratings_df = pd.DataFrame(user_movie_ratings)
     user_movie_ratings_df['movieId'] = item_factors_unstacked.T.columns
-    user_top_10 = user_movie_ratings_df.sort_values(0,ascending=False).head(10)
+    user_top_10 = user_movie_ratings_df.sort_values(0, ascending=False)\
+                                       .head(10)
     movies_df = pd.read_csv('../data/raw/movies.csv')
     user_top_10 = user_top_10.merge(movies_df, how='left', on='movieId')
-    user_top_10.drop([0,'movieId', 'genres'], axis=1, inplace=True)
+    user_top_10.drop([0, 'movieId', 'genres'], axis=1, inplace=True)
     return list(user_top_10.title)
 
 
 def get_user_cluster(user_fac):
-    gbc = load('../models/fifp_classification.joblib') 
+    gbc = load('../models/fifp_classification.joblib')
     user_cluster = gbc.predict(user_fac.reshape(1, -1))[0]
     return user_cluster
 
 
-def user_rankings():    
-    top_100 = pd.read_csv('../data/processed/top_100.csv', index_col='Unnamed: 0')
+def user_rankings():
+    top_100 = pd.read_csv('../data/processed/top_100.csv',
+                          index_col='Unnamed: 0')
     ranking_list = top_100.sample(n=50, axis=0)
     user_ratings = []
     i = 0
     j = 0
-    print('Enter a ranking from 1 (lowest) to 5 (highest) for the following movies. If you have not seen the movie, press enter.')
-    while j<9:
+    print('Enter a ranking from 1 (lowest) to 5 (highest) for \
+          the following movies. If you have not seen the movie, \
+          press enter.')
+    while j < 9:
         title = ranking_list['title']
         movieId = ranking_list['movieId']
-        #print(title.iloc[i])
         user_rating = input('{}: '.format(title.iloc[i]))
-        i +=1
+        i += 1
         j = len(user_ratings)
-        if user_rating=='':
+        if user_rating == '':
             pass
-        elif float(user_rating) in range(0,6):
+        elif float(user_rating) in range(0, 6):
             user_ratings.append((movieId.iloc[i], user_rating))
         else:
             print('Ratings must be whole numbers between 1 and 5.')
--- src/model.py
+++ src/model.py
@@ -16,6 +16,7 @@ def test_model():
     print('In Model')
     pass
 
+
 def unstack(factor_df, rank):
     """Unstacks the factor arrays into pandas dataframes with factors as columns
     and users/items as rows"""
@@ -26,18 +27,21 @@ def unstack(factor_df, rank):
                                           values='features')
     return factor_df_unstacked
 
+
 def Spark_KMeans_Eval(error):
     cluster = list(range(2, len(error)+2))
     plt.plot(cluster, error)
-    plt.title('KMeans sum of squared distances of points to their nearest center')
+    plt.title('KMeans sum of squared distances of points\
+              to their nearest center')
     plt.xlabel('Number of Clusters')
     plt.show()
     error_diff = []
-    for i in list(range(0,56)):
+    for i in list(range(0, 56)):
         diff = error[i]-error[i+1]
         error_diff.append(diff)
     cluster_diff = list(range(2, len(error_diff)+2))
     plt.plot(cluster_diff, error_diff)
-    plt.title('Difference in KMeans sum of squared distances of points to their nearest center')
+    plt.title('Difference in KMeans sum of squared distances\
+              of points to their nearest center')
     plt.xlabel('Number of Clusters')
-    plt.show();
+    plt.show()
