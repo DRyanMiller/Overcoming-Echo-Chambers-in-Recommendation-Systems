bde0d91e59ac270dea91e15a0c9bfbe1d8a38b98
D. Ryan Miller <d.ryanmiller542@gmail.com>
2019-07-28 20:21:37 -0400

    Added Kmeans code to SparkALS file and updated print statement in custom.py.

--- models/SparkALS.py
+++ models/SparkALS.py
@@ -62,3 +62,39 @@ item_factorsDF = (item_factors
                     .alias("features"))
                     .select('id', "features"))
 item_factorsDF.write.format("csv").save('s3://fp-movielens-data/item_factors.csv')
+
+#Kmeans
+
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+from numpy import array
+from math import sqrt
+
+role = 'arn:aws:iam::260329411851:role/service-role/AmazonSageMaker-ExecutionRole-20190722T132211'
+bucket = 'fp-movielens-data'
+region = 'us-east-1'
+bucket_path = 'https://s3-{}.amazonaws.com/{}'.format(region, bucket)  # The URL to access the bucket
+filename = 's3://{}/{}'.format(bucket, 'user_factors_scaled.csv')
+
+user_factors = spark.read.csv(filename, header='true', inferSchema = 'true')
+
+user_factors.head()
+
+vecAssembler = VectorAssembler(inputCols=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+                                         '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                                         '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+                                         '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+                                         '40', '41'], outputCol="features")
+new_df = vecAssembler.transform(user_factors)
+new_df.show()
+
+WSSSEs = []
+K = range(2,20)
+for k in K:
+    kmeans = KMeans(k=k, seed=1)
+    clusters = kmeans.fit(new_df.select('features'))
+    wssse = clusters.computeCost(new_df.select('features'))
+    WSSSEs.append(wssse)
+    
+kmeans = KMeans(k=18, seed=1)
+clusters = kmeans.fit(new_df.select('features'))
